5bcd438fd0af2d707ed2a1c36b5f53cea51d8ece
asif a owadud <asifaowadud@gmail.com>
2022-04-29 04:05:42 +0600

    backend ready

--- index.js
+++ index.js
@@ -13,28 +13,59 @@ app.use(express.json())
 
 
 
-
-
-
-// const uri = "mongodb+srv://ideal-CRUD-and-JWT:idealcrudandjwt123456789@cluster0.3negq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3negq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rlooh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
-  console.log('Connected to database');
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+  try {
+    await client.connect();
+    const itemsCollection = client.db("ItemsDB").collection("items");
+    console.log("Connected to MongoDB");
+
+    // get all items from db
+    app.get('/items', async (req, res) => {
+      const query = {};
+      const cursor = itemsCollection.find(query);
+      const items = await cursor.toArray();
+      // res.send(items);
+      res.send("get all items");
+    });
+
+
+    // get no of cout of all items of db
+    app.get('/itemsCount', async (req, res) => {
+      const count = await itemsCollection.estimatedDocumentCount();
+      // res.send({count});
+      res.send("{itemsCount}");
+    });
+
+
+    // get single item by id from db
+    app.get('/items/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await itemsCollection.findOne(query);
+      // res.send(item);
+      res.send("get single item");
+    });
+
+
+
+  } finally {
+
+  }
+}
+run().catch(console.dir);
 
 
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World! Its for CRUD Operations')
+  res.send('Hello World! Its for CRUD Operations')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
